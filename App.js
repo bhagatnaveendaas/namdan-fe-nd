37f33f887de9eeaf8a1f2e36af6214879ed21478
Ram suthar <ramchandra.s@gmail.com>
2021-02-22 23:31:17 +0530

    Initial structure with navigation

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigator from "./src/navigation/drawerNavigator";
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ src/navigation/stackNavigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import AshramDashboard from "../screens/AshramDashboard";
+import CountryDashboard from "../screens/CountryDashboard";
+
+const Stack = createStackNavigator();
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#9AC4F8",
+        },
+        headerTintColor: "white",
+        headerBackTitle: "Back",
+      }}
+    >
+      <Stack.Screen name="AshramDashboard" component={AshramDashboard} />
+      <Stack.Screen name="CountryDashboard" component={CountryDashboard} />
+    </Stack.Navigator>
+  );
+}
+
+export { MainStackNavigator };
