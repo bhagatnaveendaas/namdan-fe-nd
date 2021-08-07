f05171f5ce85c54ce5eda4b222a282588aa7c231
Jaideep Rana <jaideepk@thoughtworks.com>
2021-08-07 17:54:17 +0530

    Add pending satnaam report

---
+++ src/httpClient/report/pendingSatnaamAPI.js
@@ -0,0 +1,16 @@
+import { post } from "../namdanAPIClient";
+
+export const getPendingSatnaamCount = (csrfToken, f_date, t_date) => {
+  const requestBody = {
+    "page": 1,
+    "country": 2,
+    "state": 27,
+    f_date,
+    t_date,
+    "namdan": 1
+  };
+  
+  console.log(requestBody);
+
+  return post("reports/satnam_validity_expiring", requestBody, csrfToken);
+}
--- src/screens/PendingSatnaam.js
+++ src/screens/PendingSatnaam.js
@@ -1,9 +1,59 @@
-import React from "react";
-import { Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { AsyncStorage, Text } from 'react-native';
+import ReportTemplate from "../components/ReportTemplate";
+import { getPendingSatnaamCount } from "../httpClient/report/pendingSatnaamAPI";
 
 const PendingSatnaam = () => {
+  const [pendingSatnaamResponse, setPendingSatnaamResponse] = useState(null);
 
-  return <Text>Pending satnaam loaded</Text>
+  const [csrfToken, setCsrfToken] = useState("");
+
+  useEffect(() => {
+    AsyncStorage.getItem("token")
+      .then((token) => {
+        setCsrfToken(token);
+      });
+  }, [csrfToken]);
+
+  const callPendingSatnaamAPI = async (fromDate = "2017-01-03", toDate = "2021-10-03") => {
+    const response = await getPendingSatnaamCount(csrfToken, fromDate, toDate);
+    setPendingSatnaamResponse(response.data);
+  }
+
+  useEffect(() => {
+    if (pendingSatnaamResponse === null) {
+      callPendingSatnaamAPI();
+    }
+  }, [csrfToken]);
+
+  if (!pendingSatnaamResponse) return <Text>Loading data...</Text>;
+
+  const metaData = [
+    {
+      title: "Country",
+      dataKey: "country_name"
+    },
+    {
+      title: "State name",
+      dataKey: "state_name"
+    },
+    {
+      title: "District name",
+      dataKey: "district_name"
+    },
+    {
+      title: "Tehsil name",
+      dataKey: "tehsil_name"
+    },
+    {
+      title: "Total",
+      dataKey: "total",
+      numeric: true
+    }
+  ];
+
+  return <ReportTemplate metaData={metaData} data={pendingSatnaamResponse.data}
+    onDateChangeCallback={callPendingSatnaamAPI} />;
 
 };
 
