96444a21db01e4996a87a35b44416bf0d61bc531
Jaideep Rana <jaideepk@thoughtworks.com>
2021-08-17 13:14:57 +0530

    Correct the URI for naamdanReport

--- src/httpClient/report/namdanReportAPI.js
+++ src/httpClient/report/namdanReportAPI.js
@@ -1,5 +1,5 @@
 import { createRequestBasedOn, post } from "../namdanAPIClient";
 
 export const getPrathamAndSatnamCount = (csrfToken, f_date, t_date, data) => {
-  return post("reports/pratham_vs_satnam", createRequestBasedOn(data, f_date, t_date), csrfToken,"post");
+  return post("reports/namdaan_counts", createRequestBasedOn(data, f_date, t_date), csrfToken,"post");
 }
